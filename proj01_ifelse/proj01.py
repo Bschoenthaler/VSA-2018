097ba21e640d8aaf2ef6079c01ae29843909403c
Bschoenthaler <40049020+Bschoenthaler@users.noreply.github.com>
2018-06-11 15:38:51 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -20,22 +20,39 @@ print "You will graduate highschool in "+ str(Left) + " years!!"
 x= raw_input("Can I come to your graduation?")
 if x=="yes":
     print "Okay, i'll come"
+elif x=="sure":
+    "Okay, I'll come"
+elif x== "no thanks":
+    print "Okay, I won't come."
 else:
     print "Alright I won't go"
+
+
 
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
-Ur_Name = raw_input("What is your name?")
-print "Your name is " + Ur_Name[0].upper() + Ur_Name[1:].lower()
-print "I like that name."
+
+print "Hello again " + Ur_Name[0].upper() + Ur_Name[1:].lower()
+
 y= int(raw_input("What is the current day?[number]"))
 u= int(raw_input("What is the current month?[number]"))
 i= int(raw_input("What is your birth month[number]"))
 o= int(raw_input("What is your birth day[number]"))
+q= i-u
+w= 12-(u-i)
+e= o-y
+r= 30-(y-o)
 if i>u:
-    print "The number of months until your birthday is" + u-i
-else
+    print "The number of months until your birthday is " + str( q) + "."
+else:
+    print "The number of months until your birthday is " + str( w) + "."
+
+if o>=y:
+    print "The number of days until your birthday is " + str( e) + "."
+else:
+    print "The number of days until your birthday is " + str( r) + "."
+
 
 
 # If you complete extensions, describe your extensions here!
