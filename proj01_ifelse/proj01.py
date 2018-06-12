cbf6edfed275c05faf8f60af3cee13db164eac5e
Bschoenthaler <40049020+Bschoenthaler@users.noreply.github.com>
2018-06-12 09:56:02 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -20,20 +20,31 @@ print "You will graduate highschool in "+ str(Left) + " years!!"
 x= raw_input("Can I come to your graduation?")
 if x=="yes":
     print "Okay, i'll come"
+    print "Thank you for wanting to include me!!"
 elif x=="sure":
-    "Okay, I'll come"
+    print "Okay, I'll come."
+    print "Thank you for wanting to include me!!"
 elif x== "no thanks":
     print "Okay, I won't come."
+    print "At least i tried"
 else:
-    print "Alright I won't go"
-
-
+    print "Alright I won't go."
+    print "At least i tried"
+h=raw_input("Are we freinds?")
+if h== "yes":
+    print "YAY! my first freind!!"
+elif h== "sure":
+    print "YAY! my first freind!!"
+elif h=="no":
+    print "Okay, i'll look for a freind somewhere else..."
+else:
+    print "Okay, i'll look for a freind somewhere else..."
 
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
 
-print "Hello again " + Ur_Name[0].upper() + Ur_Name[1:].lower()
+print "Hello again " + Ur_Name[0].upper() + Ur_Name[1:].lower() + "!!"
 
 y= int(raw_input("What is the current day?[number]"))
 u= int(raw_input("What is the current month?[number]"))
@@ -52,6 +63,15 @@ if o>=y:
     print "The number of days until your birthday is " + str( e) + "."
 else:
     print "The number of days until your birthday is " + str( r) + "."
+
+age= raw_input("How old are you? [number]")
+if int(age)<13:
+    print "Your can watch G and PG movies only."
+elif int(age) >= 13 and int(age) < 17:
+    print "You can watch G, PG, and PG-13 movies"
+else:
+    print "You can watch G, PG, PG-13 and R rated movies"
+
 
 
 
